f5a9ca4c51c22f8ee6de17f70f2fde2fc3106097
Lexie_Murphy <Lexie.Murphy88@gmail.com>
2019-03-04 19:37:13 -0600

    initial commit

---
+++ app/data/friends.js
@@ -0,0 +1,40 @@
+var friendsData = [
+    {
+        name: "Justin Timberlake",
+        photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi-sqX05efgAhXQmq0KHeSLAjMQjRx6BAgBEAU&url=https%3A%2F%2Fwww.monmouth.edu%2Funiversity-advancement%2Four-team%2Fdevelopment%2Fperson-placeholder%2F&psig=AOvVaw2HgtyiyO4eDZStRPcCVUDs&ust=1551765180062222",
+        scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
+    },
+    {
+        name: "Jennifer Anniston",
+        photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi-sqX05efgAhXQmq0KHeSLAjMQjRx6BAgBEAU&url=https%3A%2F%2Fwww.monmouth.edu%2Funiversity-advancement%2Four-team%2Fdevelopment%2Fperson-placeholder%2F&psig=AOvVaw2HgtyiyO4eDZStRPcCVUDs&ust=1551765180062222",
+        scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    },
+    {
+        name: "Tom Hanks",
+        photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi-sqX05efgAhXQmq0KHeSLAjMQjRx6BAgBEAU&url=https%3A%2F%2Fwww.monmouth.edu%2Funiversity-advancement%2Four-team%2Fdevelopment%2Fperson-placeholder%2F&psig=AOvVaw2HgtyiyO4eDZStRPcCVUDs&ust=1551765180062222.jpg",
+        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+        name: "Joe Schmo",
+        photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi-sqX05efgAhXQmq0KHeSLAjMQjRx6BAgBEAU&url=https%3A%2F%2Fwww.monmouth.edu%2Funiversity-advancement%2Four-team%2Fdevelopment%2Fperson-placeholder%2F&psig=AOvVaw2HgtyiyO4eDZStRPcCVUDs&ust=1551765180062222",
+        scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+        name: "Jane Doe",
+        photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi-sqX05efgAhXQmq0KHeSLAjMQjRx6BAgBEAU&url=https%3A%2F%2Fwww.monmouth.edu%2Funiversity-advancement%2Four-team%2Fdevelopment%2Fperson-placeholder%2F&psig=AOvVaw2HgtyiyO4eDZStRPcCVUDs&ust=1551765180062222",
+        scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5]
+    },
+    {
+        name: "Leonardo DiCaprio",
+        photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi-sqX05efgAhXQmq0KHeSLAjMQjRx6BAgBEAU&url=https%3A%2F%2Fwww.monmouth.edu%2Funiversity-advancement%2Four-team%2Fdevelopment%2Fperson-placeholder%2F&psig=AOvVaw2HgtyiyO4eDZStRPcCVUDs&ust=1551765180062222",
+        scores: [2, 4, 2, 4, 2, 3, 4, 4, 4, 3]
+    },
+    {
+        name: "Brittany Spears",
+        photo: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi-sqX05efgAhXQmq0KHeSLAjMQjRx6BAgBEAU&url=https%3A%2F%2Fwww.monmouth.edu%2Funiversity-advancement%2Four-team%2Fdevelopment%2Fperson-placeholder%2F&psig=AOvVaw2HgtyiyO4eDZStRPcCVUDs&ust=1551765180062222",
+        scores: [5, 4, 5, 4, 5, 3, 3, 3, 2, 4]
+    }
+];
+
+
+module.exports = friendsData;
